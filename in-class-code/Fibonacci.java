aad1a21b2336cd207f07c4db88f61ce77be454f1
Mark Bonicillo <markabonicillo@gmail.com>
2022-10-07 06:39:13 -0700

    Add Fibonacci solutions

---
+++ in-class-code/Fibonacci.java
@@ -0,0 +1,57 @@
+public class Fibonacci {
+   public static void main(String[] args) {
+      printFibonacciV1();
+      System.out.println();
+      printFibonacciV2();
+      System.out.println();
+      printFibonacciV3();
+      System.out.println();
+   }
+   
+   public static void printFibonacciV1() {
+      int firstNum = 0;
+      int secondNum = 1;
+      int sum = firstNum + secondNum;
+      
+      for (int i = 1; i <= 12; i++) {
+         System.out.print(sum + " ");
+         sum = firstNum + secondNum;
+         firstNum = secondNum;
+         secondNum = sum;
+      }
+   }
+  
+  public static void printFibonacciV2() {
+      int firstNum = 0;
+      int secondNum = 1;
+      int sum = firstNum + secondNum;
+      int count = 12;
+      
+      while (count >= 1) {
+         System.out.print(sum + " ");
+         sum = firstNum + secondNum;
+         firstNum = secondNum;
+         secondNum = sum;
+         
+         count--;
+      }
+   }
+   
+   public static int TOTAL_NUMBERS = 12;
+   public static int FIRST_NUM = 0;
+   public static int SECOND_NUM = 1;
+   public static int SUM = FIRST_NUM + SECOND_NUM;
+   public static void printFibonacciV3() {      
+      if (TOTAL_NUMBERS == 0) {
+         System.out.print("");
+      } else {
+         System.out.print(SUM + " ");
+         SUM = FIRST_NUM + SECOND_NUM;
+         FIRST_NUM = SECOND_NUM;
+         SECOND_NUM = SUM;
+         TOTAL_NUMBERS--;
+         printFibonacciV3();  
+      }
+   }
+
+}
